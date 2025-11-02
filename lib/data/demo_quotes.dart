5e75f8bede8d6b908b226a3c7845f80df66dce5e
Rifqi Reissal Arasy <rifqiarasy@gmail.com>
2025-11-02 19:09:20 +0700

    initial-backend

---
+++ lib/data/demo_quotes.dart
@@ -0,0 +1,14 @@
+import '/models/quote.dart';
+
+final List<Quote> demoQuotes = [
+  Quote(
+    text: "wleeeeeeeeeeeeeeee",
+    author: "Damar",
+    category: "life",
+  ),
+  Quote(
+    text: "sudahkah anda crit hari ini",
+    author: "ko kevin",
+    category: "motivasi",
+  ),
+];
---
+++ lib/models/quote.dart
@@ -0,0 +1,9 @@
+class Quote {
+
+final String text;
+final String author;
+final String category;
+
+  Quote({required this.text, required this.author, required this.category});
+  
+}
